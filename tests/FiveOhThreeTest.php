dcf701eb7087f000d19d3bccc77dfc5942def208
Mickael GOETZ <contact@mickael-goetz.com>
2019-09-06 15:49:00 +0200

    add checkAndThrow to extend usage to non-web scripts

---
+++ tests/FiveOhThreeTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests;
+
+
+use Mesavolt\FiveOhThree\LockFileException;
+use Mesavolt\FiveOhThree\LockGuard;
+use PHPUnit\Framework\TestCase;
+
+class FiveOhThreeTest extends TestCase
+{
+    protected function tearDown()
+    {
+        parent::tearDown();
+
+        // ensure each test gets a clean env
+        unset($_ENV['FIVEOHTHREE_IGNORE_LOCK']);
+    }
+
+    public function test_lockFileExists()
+    {
+        $this->assertTrue(LockGuard::lockFileExists(__FILE__), 'Lock file should be detected');
+        $this->assertFalse(LockGuard::lockFileExists('/i-do-not-exists'), "Lock file shouldn't be detected");
+
+        $_ENV['FIVEOHTHREE_IGNORE_LOCK'] = 'true';
+        $this->assertFalse(LockGuard::lockFileExists('/i-do-not-exists'), 'Lock file should be ignored');
+    }
+
+    public function test_checkAndThrow()
+    {
+        $this->expectException(LockFileException::class);
+        LockGuard::checkAndThrow(['lock_path' => __FILE__]);
+    }
+}
