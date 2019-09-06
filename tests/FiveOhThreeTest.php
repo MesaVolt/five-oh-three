05f87863f85adfbd13cba6733da1a6543c7ae23b
Mickael GOETZ <MrMitch@users.noreply.github.com>
2019-09-06 17:31:28 +0200

    tests: fix typo
    Co-Authored-By: Quentin Stoeckel <stoeckel.quentin@gmail.com>

--- tests/FiveOhThreeTest.php
+++ tests/FiveOhThreeTest.php
@@ -20,7 +20,7 @@ class FiveOhThreeTest extends TestCase
     public function test_lockFileExists()
     {
         $this->assertTrue(LockGuard::lockFileExists(__FILE__), 'Lock file should be detected');
-        $this->assertFalse(LockGuard::lockFileExists('/i-do-not-exists'), "Lock file shouldn't be detected");
+        $this->assertFalse(LockGuard::lockFileExists('/i-do-not-exist'), "Lock file shouldn't be detected");
 
         $_ENV['FIVEOHTHREE_IGNORE_LOCK'] = 'true';
         $this->assertFalse(LockGuard::lockFileExists('/i-do-not-exists'), 'Lock file should be ignored');
